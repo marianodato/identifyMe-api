ef2144c484d25cabb91d24c4dd2449114d992904
marianodato <marianodato@gmail.com>
2018-05-09 20:56:50 -0300

    Add log

--- grails-app/controllers/webserver/PingController.groovy
+++ grails-app/controllers/webserver/PingController.groovy
@@ -5,6 +5,7 @@ import grails.converters.JSON
 class PingController {
 
     def index() {
+        log.info("Entered Ping Controller!")
         def resp = [:]
         resp.ping = "pong"
         render resp as JSON
