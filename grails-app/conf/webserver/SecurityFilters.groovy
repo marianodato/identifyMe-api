afe578ae66d058eb61a78ae7c62aeab1b51c8a72
marianodato <marianodato@gmail.com>
2018-05-13 11:51:09 -0300

    Rollback filters change

--- grails-app/conf/webserver/SecurityFilters.groovy
+++ grails-app/conf/webserver/SecurityFilters.groovy
@@ -7,7 +7,7 @@ class SecurityFilters {
     def filters = {
         switchToHttps(controller:'*', action:'*') {
             before = {
-                if (!(request.isSecure() || request.getHeader('X-Forwarded-Proto')?.toLowerCase() == 'https') && !Environment.isDevelopmentMode() && Environment.getCurrent()!=Environment.PRODUCTION) {
+                if (!(request.isSecure() || request.getHeader('X-Forwarded-Proto')?.toLowerCase() == 'https') && !Environment.isDevelopmentMode()) {
                     def url = "https://" + request.serverName + request.forwardURI + (request.queryString ? "?" + request.queryString : "")
                     redirect(url: url, permanent: true)
                     return false
